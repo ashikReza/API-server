0eb1094f2a5a6a6496cdbc8265430b325e884257
Ashik044 <rezaashik044@gmail.com>
2024-04-11 18:58:00 +0600

    done

--- main.js
+++ main.js
@@ -8,8 +8,8 @@ const app = express();
 
 require("dotenv/config");
 
-const authJwt = require("./helpers/jwt");
-const errorHandler = require("./helpers/error-handler");
+const authJwt = require("./helpers/jwt.js");
+const errorHandler = require("./helpers/error-handler.js");
 
 app.use(cors());
 app.options("*", cors());
@@ -23,10 +23,10 @@ app.use("/public/uploads", express.static(__dirname + "/public/uploads"));
 app.use(errorHandler);
 
 const api = process.env.API_URL;
-const categoriesRoute = require("./routes/categories");
-const productRoute = require("./routes/products");
-const userRoute = require("./routes/users");
-const orderRoute = require("./routes/orders");
+const categoriesRoute = require("./routes/categories.js");
+const productRoute = require("./routes/products.js");
+const userRoute = require("./routes/users.js");
+const orderRoute = require("./routes/orders.js");
 
 // Routes
 
