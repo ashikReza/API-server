3e58dbee4a1f8b5d4ee23ac8c83845faf62ff983
Ashik044 <rezaashik044@gmail.com>
2024-04-12 23:04:35 +0600

    categorites all done

--- routes/categories.js
+++ routes/categories.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const router = express();
 const multer = require("multer");
 
@@ -12,13 +13,18 @@ router.get("/", async (req, res) => {
       return res.status(500).json({ success: false });
     }
 
-    // Modify the image URLs to include the base path
+    // Modify the image and icon URLs to include the base path
     const categoriesWithBasePath = categoryList.map((category) => {
       return {
         ...category._doc,
         image: category.image
           ? `${req.protocol}://${req.get("host")}/public/uploads/${
               category.image
+            }`
+          : null,
+        icon: category.icon
+          ? `${req.protocol}://${req.get("host")}/public/uploads/${
+              category.icon
             }`
           : null,
       };
@@ -42,13 +48,16 @@ router.get("/:id", async (req, res) => {
       });
     }
 
-    // Modify the image URL to include the base path
+    // Modify the image and icon URL to include the base path
     const categoryWithBasePath = {
       ...category._doc,
       image: category.image
         ? `${req.protocol}://${req.get("host")}/public/uploads/${
             category.image
           }`
+        : null,
+      icon: category.icon
+        ? `${req.protocol}://${req.get("host")}/public/uploads/${category.icon}`
         : null,
     };
 
@@ -59,85 +68,116 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-// Multer configuration
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "./public/uploads/"); // Set the destination folder
+    cb(null, "./public/uploads/");
   },
   filename: function (req, file, cb) {
-    cb(null, file.originalname); // Set the file name
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(
+      null,
+      file.fieldname + "-" + uniqueSuffix + path.extname(file.originalname)
+    );
   },
 });
 const upload = multer({ storage: storage });
 
-router.post("/", upload.single("image"), async (req, res) => {
-  try {
-    const existingCategory = await Category.findOne({ name: req.body.name });
+router.post(
+  "/",
+  upload.fields([
+    { name: "image", maxCount: 1 },
+    { name: "icon", maxCount: 1 },
+  ]),
+  async (req, res) => {
+    try {
+      const existingCategory = await Category.findOne({ name: req.body.name });
 
-    if (existingCategory) {
-      return res.status(400).send("Category name already in use");
-    }
+      if (existingCategory) {
+        return res.status(400).send("Category name already in use");
+      }
 
-    const category = new Category({
-      name: req.body.name,
-      icon: req.body.icon,
-      color: req.body.color,
-      image: req.file ? req.file.filename : null, // Use uploaded image filename if available
-    });
-
-    const savedCategory = await category.save();
-
-    if (!savedCategory) {
-      return res.status(500).send("Failed to create category");
-    }
-
-    // Construct the image URL
-    savedCategory.image = `${req.protocol}://${req.get(
-      "host"
-    )}/public/uploads/${savedCategory.image}`;
-
-    res.status(201).send(savedCategory);
-  } catch (error) {
-    console.error("Error creating category:", error);
-    res.status(500).send("Internal server error");
-  }
-});
-
-router.put("/:id", upload.single("image"), async (req, res) => {
-  try {
-    const existingCategory = await Category.findOne({
-      name: req.body.name,
-      _id: { $ne: req.params.id },
-    });
-
-    if (existingCategory) {
-      return res.status(400).send("Category name already in use");
-    }
-
-    const category = await Category.findByIdAndUpdate(
-      req.params.id,
-      {
+      const category = new Category({
         name: req.body.name,
-        icon: req.body.icon,
+        icon: req.files["icon"] ? req.files["icon"][0].filename : null,
         color: req.body.color,
-        image: req.file ? req.file.filename : req.body.image, // Update image if uploaded, else use existing image
-      },
-      { new: true }
-    );
+        image: req.files["image"] ? req.files["image"][0].filename : null,
+      });
 
-    if (!category) return res.status(404).send("Category cannot be updated");
+      const savedCategory = await category.save();
 
-    // Construct the image URL
-    category.image = `${req.protocol}://${req.get("host")}/public/uploads/${
-      category.image
-    }`;
+      if (!savedCategory) {
+        return res.status(500).send("Failed to create category");
+      }
 
-    res.send(category);
-  } catch (error) {
-    console.error("Error updating category:", error);
-    res.status(500).send("Internal server error");
+      // Construct the image URL
+      savedCategory.image = savedCategory.image
+        ? `${req.protocol}://${req.get("host")}/public/uploads/${
+            savedCategory.image
+          }`
+        : null;
+      savedCategory.icon = savedCategory.icon
+        ? `${req.protocol}://${req.get("host")}/public/uploads/${
+            savedCategory.icon
+          }`
+        : null;
+
+      res.status(201).send(savedCategory);
+    } catch (error) {
+      console.error("Error creating category:", error);
+      res.status(500).send("Internal server error");
+    }
   }
-});
+);
+
+router.put(
+  "/:id",
+  upload.fields([
+    { name: "image", maxCount: 1 },
+    { name: "icon", maxCount: 1 },
+  ]),
+  async (req, res) => {
+    try {
+      const existingCategory = await Category.findOne({
+        name: req.body.name,
+        _id: { $ne: req.params.id },
+      });
+
+      if (existingCategory) {
+        return res.status(400).send("Category name already in use");
+      }
+
+      const category = await Category.findByIdAndUpdate(
+        req.params.id,
+        {
+          name: req.body.name,
+          icon: req.files["icon"] ? req.files["icon"][0].filename : null,
+          color: req.body.color,
+          image: req.files["image"]
+            ? req.files["image"][0].filename
+            : req.body.image,
+        },
+        { new: true }
+      );
+
+      if (!category) return res.status(404).send("Category cannot be updated");
+
+      // Construct the image and icon URLs
+      category.image = category.image
+        ? `${req.protocol}://${req.get("host")}/public/uploads/${
+            category.image
+          }`
+        : null;
+      category.icon = category.icon
+        ? `${req.protocol}://${req.get("host")}/public/uploads/${category.icon}`
+        : null;
+
+      res.send(category);
+    } catch (error) {
+      console.error("Error updating category:", error);
+      res.status(500).send("Internal server error");
+    }
+  }
+);
 
 router.delete("/:id", (req, res) => {
   Category.findByIdAndRemove(req.params.id)
